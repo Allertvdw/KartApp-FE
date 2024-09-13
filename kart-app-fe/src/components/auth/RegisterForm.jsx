1d64b86409be59af360042d25481e6638930b6b7
Allertvdw <allertvdw@hotmail.com>
2024-09-13 14:30:46 +0200

    Linked registration endpoint to frontend

--- kart-app-fe/src/components/auth/RegisterForm.jsx
+++ kart-app-fe/src/components/auth/RegisterForm.jsx
@@ -1,11 +1,46 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function RegisterForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSumbit = async (e) => {
+    e.preventDefault();
+    await registerRequest();
+  };
+
+  const registerRequest = async () => {
+    try {
+      await fetch("https://localhost:7197/register", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+    } catch (error) {
+      console.error("Error during registration: ", error);
+      navigate("/register");
+    }
+  };
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm">
+      <form
+        className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm"
+        onSubmit={handleSumbit}
+      >
         <h1 className="text-2xl font-bold text-center mb-6">Register</h1>
         <div className="inputs space-y-4">
           <div className="email">
             <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               type="email"
               placeholder="Email"
               className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -13,6 +48,8 @@ export default function RegisterForm() {
           </div>
           <div className="password">
             <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               type="password"
               placeholder="Password"
               className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -22,7 +59,7 @@ export default function RegisterForm() {
         <button className="w-full bg-blue-500 text-white p-3 rounded-lg mt-6 hover:bg-blue-600">
           Register
         </button>
-      </div>
+      </form>
     </div>
   );
 }
