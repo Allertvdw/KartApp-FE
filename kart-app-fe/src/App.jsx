e52a136eb9acfdf3bcc6a138be11e2c847507fe4
Allertvdw <allertvdw@hotmail.com>
2024-09-13 13:12:24 +0200

    Added login page and routing

--- kart-app-fe/src/App.jsx
+++ kart-app-fe/src/App.jsx
@@ -1,9 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import LoginForm from "./components/auth/LoginForm";
 import RegisterForm from "./components/auth/RegisterForm";
 
 export default function App() {
   return (
-    <div>
-      <RegisterForm />
-    </div>
+    <>
+      <Routes>
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/login" element={<LoginForm />} />
+      </Routes>
+    </>
   );
 }
