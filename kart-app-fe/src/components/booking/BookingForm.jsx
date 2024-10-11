ed04d06133b51b629f317d4ed5b1972290c64744
Allertvdw <allertvdw@hotmail.com>
2024-10-11 12:46:55 +0200

    Added booking logic with fetch sessions

--- kart-app-fe/src/components/booking/BookingForm.jsx
+++ kart-app-fe/src/components/booking/BookingForm.jsx
@@ -1,18 +1,61 @@
-import { useState } from "react";
-import { timeslots } from "../../assets/timeslots";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ToastNotification from "../notifications/ToastNotification";
 
 export default function BookingForm() {
   const [date, setDate] = useState("");
   const [peopleCount, setPeopleCount] = useState(1);
-  const [timeslot, setTimeslot] = useState("");
+  const [selectedSession, setSelectedSession] = useState("");
+  const [sessions, setSessions] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (date) {
+      fetchAvailableSessions(date);
+    }
+  }, [date]);
+
+  const fetchAvailableSessions = async () => {
+    const response = await fetch(
+      `https://localhost:7197/api/Session?date=${date}`
+    );
+    const data = await response.json();
+    setSessions(data);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log({
-      date,
-      peopleCount,
-      timeslot,
-    });
+
+    if (!selectedSession) {
+      ToastNotification("error", "Please select a timeslot.");
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const response = await fetch("https://localhost:7197/api/Booking", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          sessionId: selectedSession,
+          peopleCount,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to book session");
+      }
+
+      ToastNotification("success", "Session booked successfully.");
+    } catch (error) {
+      ToastNotification("error", error.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -33,6 +76,35 @@ export default function BookingForm() {
           />
         </div>
         <div>
+          <label htmlFor="selectedSession" className="block mb-2">
+            Session:
+          </label>
+          <select
+            id="selectedSession"
+            value={selectedSession}
+            onChange={(e) => setSelectedSession(e.target.value)}
+            className="border p-2 w-full"
+            required
+          >
+            <option value="" disabled>
+              Select a time slot
+            </option>
+            {sessions.map((timeslot) => (
+              <option key={timeslot.id} value={timeslot.id}>
+                {new Date(timeslot.startTime).toLocaleTimeString([], {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}{" "}
+                -{" "}
+                {new Date(timeslot.endTime).toLocaleTimeString([], {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
           <label htmlFor="peopleCount" className="block mb-2">
             Number of people:
           </label>
@@ -45,27 +117,6 @@ export default function BookingForm() {
             className="border p-2 w-full"
             required
           />
-        </div>
-        <div>
-          <label htmlFor="timeSlot" className="block mb-2">
-            Time slot:
-          </label>
-          <select
-            id="timeSlot"
-            value={timeslot}
-            onChange={(e) => setTimeslot(e.target.value)}
-            className="border p-2 w-full"
-            required
-          >
-            <option value="" disabled>
-              Select a time slot
-            </option>
-            {timeslots.map((slot, index) => (
-              <option key={index} value={slot}>
-                {slot}
-              </option>
-            ))}
-          </select>
         </div>
         <button
           type="submit"
