dd13b885a8fa2b7683376b8a56fda7c68cf0be6c
Allertvdw <allertvdw@hotmail.com>
2024-10-31 15:10:33 +0100

    Added admin dashboard

--- kart-app-fe/src/App.jsx
+++ kart-app-fe/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import LoginForm from "./components/auth/LoginForm";
 import RegisterForm from "./components/auth/RegisterForm";
 import { ToastContainer } from "react-toastify";
@@ -9,11 +9,16 @@ import Home from "./components/home/Home";
 import BookingForm from "./components/booking/BookingForm";
 import BookingConfirmation from "./components/booking/BookingConfirmation";
 import UserOverview from "./components/admin/users/UserOverview";
+import Dashboard from "./components/admin/Dashboard";
 
 export default function App() {
+  const location = useLocation();
+  const isAdminPage = location.pathname.startsWith("/admin");
+
   return (
     <>
-      <Header />
+      {!isAdminPage && <Header />}
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<RegisterForm />} />
@@ -23,9 +28,11 @@ export default function App() {
           path="/booking/confirmation/:bookingId"
           element={<BookingConfirmation />}
         />
-        <Route path="/users" element={<UserOverview />} />
+        <Route path="/admin" element={<Dashboard />} />
+        <Route path="/admin/users" element={<UserOverview />} />
       </Routes>
-      <Footer />
+
+      {!isAdminPage && <Footer />}
       <ToastContainer stacked position="top-center" />
     </>
   );
