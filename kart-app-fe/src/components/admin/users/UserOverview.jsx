28b2ff4fe53a96dbaf33032e8a1f40544efb6d87
Allertvdw <allertvdw@hotmail.com>
2024-11-07 16:15:43 +0100

    Added session overview page

--- kart-app-fe/src/components/admin/users/UserOverview.jsx
+++ kart-app-fe/src/components/admin/users/UserOverview.jsx
@@ -24,7 +24,7 @@ export default function UserOverview() {
   };
 
   return (
-    <div className="max-w-7xl mx-auto p-4 pt-20">
+    <div className="max-w-7xl mx-auto p-4 pt-10">
       <h1 className="text-3xl text-center font-bold mb-10">User Overview</h1>
       <table className="w-full bg-white border border-gray-300">
         <thead>
