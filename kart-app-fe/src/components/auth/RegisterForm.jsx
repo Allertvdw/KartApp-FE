345371e5bcdb6f7d356e960076d5aa688b32a252
Allertvdw <allertvdw@hotmail.com>
2024-12-11 14:38:14 +0100

    Updated register page

--- kart-app-fe/src/components/auth/RegisterForm.jsx
+++ kart-app-fe/src/components/auth/RegisterForm.jsx
@@ -3,8 +3,11 @@ import { useNavigate } from "react-router-dom";
 import ToastNotification from "../notifications/ToastNotification";
 
 export default function RegisterForm() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
   const navigate = useNavigate();
 
   const handleSumbit = async (e) => {
@@ -20,8 +23,11 @@ export default function RegisterForm() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
+          firstName: firstName,
+          lastName: lastName,
           email: email,
           password: password,
+          phoneNumber: phoneNumber,
         }),
       });
 
@@ -52,6 +58,24 @@ export default function RegisterForm() {
       >
         <h1 className="text-2xl font-bold text-center mb-6">Register</h1>
         <div className="inputs space-y-4">
+          <div className="firstName">
+            <input
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              type="name"
+              placeholder="First name"
+              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500"
+            />
+          </div>
+          <div className="lastName">
+            <input
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              type="name"
+              placeholder="Last name"
+              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500"
+            />
+          </div>
           <div className="email">
             <input
               value={email}
@@ -67,6 +91,15 @@ export default function RegisterForm() {
               onChange={(e) => setPassword(e.target.value)}
               type="password"
               placeholder="Password"
+              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500"
+            />
+          </div>
+          <div className="phoneNumber">
+            <input
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              type="phone"
+              placeholder="Phone number"
               className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500"
             />
           </div>
