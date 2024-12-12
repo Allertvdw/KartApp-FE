fb91982c520512609c3e71673d601063fa835fcd
Allertvdw <allertvdw@hotmail.com>
2024-12-12 16:14:35 +0100

    Adjusted session details

--- kart-app-fe/src/components/admin/sessions/SessionDetails.jsx
+++ kart-app-fe/src/components/admin/sessions/SessionDetails.jsx
@@ -43,11 +43,24 @@ export default function SessionDetails() {
   return (
     <div className="max-w-7xl mx-auto p-4 pt-10">
       <h1 className="text-3xl text-center font-bold mb-6">Session Details</h1>
+      <div className="mb-6 text-center">
+        <p className="text-lg font-semibold">
+          Date:{" "}
+          {sessionDetails.length > 0 &&
+            formatDate(sessionDetails[0].sessionDate)}
+        </p>
+        <p className="text-lg font-semibold">
+          Time:{" "}
+          {sessionDetails.length > 0 &&
+            formatTime(sessionDetails[0].sessionStartTime)}{" "}
+          -{" "}
+          {sessionDetails.length > 0 &&
+            formatTime(sessionDetails[0].sessionEndTime)}
+        </p>
+      </div>
       <table className="w-full bg-white border border-gray-300">
         <thead>
           <tr className="bg-gray-100 text-left">
-            <th className="p-2 border-b">Date</th>
-            <th className="p-2 border-b">Time</th>
             <th className="p-2 border-b">Kart Number</th>
             <th className="p-2 border-b">Name</th>
             <th className="p-2 border-b">Booking ID</th>
@@ -57,15 +70,10 @@ export default function SessionDetails() {
           {sessionDetails.length > 0 ? (
             sessionDetails.map((details) => (
               <tr key={details.kartNumber} className="hover:bg-gray-50">
-                <td className="p-2 border-b">
-                  {formatDate(details.sessionDate)}
-                </td>
-                <td className="p-2 border-b">
-                  {formatTime(details.sessionStartTime)} -{" "}
-                  {formatTime(details.sessionEndTime)}
-                </td>
                 <td className="p-2 border-b">{details.kartNumber}</td>
-                <td className="p-2 border-b">{details.firstName}</td>
+                <td className="p-2 border-b">
+                  {details.firstName} {details.lastName}
+                </td>
                 <td className="p-2 border-b">{details.bookingId}</td>
               </tr>
             ))
